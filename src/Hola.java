91935e4c4a2a154e1735d9c980c55330822d7648
Chavezg29 <alucha4629@ieselcaminas.org>
2022-02-15 13:12:10 +0100

    Hola Mundo

--- src/Hola.java
+++ src/Hola.java
@@ -2,8 +2,7 @@ public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Primera refactorización" );
-        System.out.println("Segunda refactorización" );
-        System.out.println("Nueva refactorización" );
+
 
     }
 }
